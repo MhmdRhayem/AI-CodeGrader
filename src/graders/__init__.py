a8f476741839ee24d29b9bd774ffef76dfa4076b
MhmdRhayem <10121011@mu.edu.lb>
2026-01-13 06:48:31 +0200

    Implement base grader architecture
    - Create abstract BaseGrader class for all strategies
    - Define grader interface and abstract methods
    - Export graders from module

--- src/graders/__init__.py
+++ src/graders/__init__.py
@@ -4,5 +4,14 @@ Grading strategy implementations.
 
 from src.graders.base_grader import BaseGrader
 from src.graders.cot_grader import ChainOfThoughtGrader
+from src.graders.few_shot_cot_grader import FewShotCoTGrader
+from src.graders.voting_grader import VotingGrader
+from src.graders.evaluator_optimizer_grader import EvaluatorOptimizerGrader
 
-__all__ = ["BaseGrader", "ChainOfThoughtGrader"]
+__all__ = [
+    "BaseGrader",
+    "ChainOfThoughtGrader",
+    "FewShotCoTGrader",
+    "VotingGrader",
+    "EvaluatorOptimizerGrader",
+]
